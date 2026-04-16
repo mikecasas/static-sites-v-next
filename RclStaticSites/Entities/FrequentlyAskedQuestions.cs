3f99f5fdf4ff2a906ed4ff4cd6e6a2a5edea297f
Mike Casas <mike.casas.public@gmail.com>
2026-04-16 10:39:20 -0400

    Updates.

--- RclStaticSites/Entities/FrequentlyAskedQuestions.cs
+++ RclStaticSites/Entities/FrequentlyAskedQuestions.cs
@@ -1,12 +1,8 @@
-<<<<<<< HEAD:RclStaticSites/Entities/FrequentlyAskedQuestions.cs
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace RclStaticSites.Entities
-=======
-﻿namespace StaticSitesV10.Entities
->>>>>>> origin/master-2:StaticSitesV10/Entities/FrequentlyAskedQuestions.cs
 {
     public  class FrequentlyAskedQuestions
     {
